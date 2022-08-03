cf8a4474a4f851d042228413ee6656fa4df903ac
uu <110501303+5-uu@users.noreply.github.com>
2022-08-03 00:01:10 -0400

    Add files via upload

---
+++ important/weight.lua
@@ -0,0 +1,15 @@
+getgenv().weighted_random = function(weights)
+    local summ = 0
+    for i, weight in pairs (weights) do
+        summ = summ + weight
+    end
+    if summ == 0 then return end
+    local value = summ*math.random ()
+    summ = 0
+    for i, weight in pairs (weights) do
+        summ = summ + weight
+        if value <= summ then
+            return i, weight
+        end
+    end
+end
